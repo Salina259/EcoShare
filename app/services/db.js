67a27bc37ca631690e6171c326363cede2617d14
Salina259 <gurungs25@roehampton.ac.uk>
2026-05-03 13:59:43 +0100

    My fixes before sync

--- app/services/db.js
+++ app/services/db.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 
 const mysql = require('mysql2/promise');
+const fs = require("fs");
+
+const isRunningInDocker = fs.existsSync("/.dockerenv");
+const dbHost = isRunningInDocker
+  ? (process.env.DB_CONTAINER || process.env.MYSQL_HOST || "localhost")
+  : (process.env.MYSQL_HOST || "localhost");
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: process.env.DB_CONTAINER,
+    host: dbHost,
     port: process.env.DB_PORT,
     user: process.env.MYSQL_ROOT_USER,
     password: process.env.MYSQL_ROOT_PASSWORD,
